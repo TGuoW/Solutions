36a6a377b611c7a19e44529a763cd049401ad819
TGuoW <15768620356@163.com>
2019-04-25 09:31:47 +0800

    add power-of-three

---
+++ leetcode/326.power-of-three.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    while (n % 3 === 0 && n !== 0) {
+        n /= 3
+    }
+    return n === 1
+};
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    return n > 0 && 1162261467 % n == 0;
+};
